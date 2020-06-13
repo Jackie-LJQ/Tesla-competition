a50719699444cc1dbe2141a9a1c3476454484b42
liu <liujiaqier@gmail.com>
2020-06-13 00:03:21 -0700

    extract features of diff type site

---
+++ extract_features.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 12 23:09:53 2020
+
+@author: liu
+"""
+import pandas as pd
+# extract feature of a-type site
+
+def find_feature(t = 'a'):
+    df = pd.read_csv('data/site.csv')
+    dfa1 = df.where(df.site_type == t).dropna()
+    dfa = dfa1[['var_name', 'var_value']]
+    df1 = dfa.where(dfa.var_name == 'charging_experience_v1').dropna().reset_index()
+    feature = pd.DataFrame({'charging_experience_v1':df1.var_value})
+    for name in df.var_name.unique()[1:]:
+        temp = dfa.where(dfa.var_name == name).dropna().reset_index()
+        temp = temp.rename(columns = {'var_value':name})
+        feature = pd.concat([feature, temp[name]], axis=1)
+    feature.to_csv('data'+t+'_feature.csv', index = False)
+# similiarly find feature of b and c site. 
+#find_feature('a')
+#find_feature('b')
+#find_feature('c')
+
+df1 = pd.read_csv('data/a_feature.csv')  
+df2 = pd.read_csv('data/b_feature.csv')
+df3 = pd.read_csv('data/c_feature.csv')
+    
+df1 
+    
+    
